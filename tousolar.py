b2ef98946f2f6299a83b6540a55c2e8363aabf5e
Paul Laborde <paullaborde@laposte.net>
2024-06-15 14:47:51 +0200

    basic workflow simulation before graph

--- tousolar.py
+++ tousolar.py
@@ -1,14 +1,109 @@
+"""
+Simulateur Tousolar sur Streamlit
+
+Simuler sa production solaire vs sa consommation réelle extraite d'Enedis
+
+There is also an accompanying png and pdf version
+
+https://tousolar.com
+
+v1.0
+15 Juin 2024
+
+Author:
+    paul.laborde@laposte.net
+"""
+
 import streamlit as st
 import pandas as pd
 from io import StringIO
+import requests
 
+# ------------------------------------------------------
+# Init
+# ------------------------------------------------------
 
-# https://mon-compte-particulier.enedis.fr/
+st.set_page_config(
+     page_title='Simulateur Tousolar',
+     layout="wide",
+     initial_sidebar_state="expanded",
+)
 
+# ------------------------------------------------------
+# 1. Conso
+# ------------------------------------------------------
+st.text('1. Consommation réelle')
 
-
-uploaded_file = st.file_uploader("Enedis real conso file")
+uploaded_file = st.file_uploader("Chargez votre consommation réelle depuis https://mon-compte-particulier.enedis.fr/")
 if uploaded_file is not None:
-    df = pd.read_csv(uploaded_file, sep=';')
+    df_conso = pd.read_csv(uploaded_file, sep=';')
 
-    st.write(df)
+    st.write(df_conso)
+
+# ------------------------------------------------------
+# 2. Adress
+# ------------------------------------------------------
+st.text('2. Emplacement projet solaire')
+
+address = st.text_input("Indiquez l'adresse du projet")
+
+url_geocoding = f'https://api.mapbox.com/geocoding/v5/mapbox.places/{address}.json?&access_token={st.secrets["mapbox_token"]}'
+r = requests.get(url_geocoding)
+results = r.json()
+
+if len(results['features']) >0:
+    feature = results['features'][0]
+    tmp_lat = feature["center"][1]
+    tmp_lng = feature["center"][0]
+
+    url_insight = f'https://solar.googleapis.com/v1/buildingInsights:findClosest?location.latitude={tmp_lat}&location.longitude={tmp_lng}&requiredQuality=HIGH&key={st.secrets["google_solarapi_key"]}'
+
+    r = requests.get(url_insight)
+    results = r.json()
+
+    roof_segments = len(results['solarPotential']['roofSegmentStats'])
+    tmp_pitch = results['solarPotential']['roofSegmentStats'][0]['pitchDegrees']
+    tmp_azimuth = results['solarPotential']['roofSegmentStats'][0]['azimuthDegrees']
+
+    st.write('Adresse trouvée :', feature['place_name'])
+    st.write(f'Nombre de pan de toiture exploitables : {roof_segments}')
+    st.write(f'Pente du pan principal : {int(tmp_pitch)}°')
+    st.write(f'Orientation du pan principal : {int(tmp_azimuth)}°')
+
+
+    data = pd.DataFrame({
+        'latitude': [tmp_lat],
+        'longitude': [tmp_lng]
+    })
+    
+    st.map(data, zoom=17)
+
+
+# ------------------------------------------------------
+# 3. Production
+# ------------------------------------------------------
+
+    url = f"https://re.jrc.ec.europa.eu/api/v5_2/seriescalc?lat={tmp_lat}&lon={tmp_lng}&raddatabase=PVGIS-SARAH&pvcalculation=1&peakpower=1.0&loss=14.0&angle={tmp_pitch}&aspect={tmp_azimuth}&outputformat=json"
+    r = requests.get(url)
+    result = r.json()
+
+    # Mean per hour on years
+    tmp_df = pd.DataFrame(result['outputs']['hourly'])
+
+    # split date & time to compare with linky
+    tmp_df['datetime'] = pd.to_datetime(tmp_df['time'], format='%Y%m%d:%H%M')
+    tmp_df['month'] = pd.DatetimeIndex(tmp_df['datetime']).month
+    tmp_df['day'] = pd.DatetimeIndex(tmp_df['datetime']).day
+    tmp_df['hour'] = pd.DatetimeIndex(tmp_df['datetime']).hour
+    tmp_df['minute'] = pd.DatetimeIndex(tmp_df['datetime']).minute
+
+    # clean columns
+    tmp_df.drop(columns=['time', 'datetime', 'G(i)', 'H_sun', 'T2m', 'WS10m', 'Int'], inplace=True)
+
+    # mean power over years
+    mean = tmp_df.groupby(['month', 'day', 'hour', 'minute']).mean().to_dict()
+    power = [{'month':x[0], 'day':x[1], 'hour':x[2], 'minute':x[3], 'power':mean['P'][x]} for x in mean['P']]
+    prod_df = pd.DataFrame(power)
+
+    st.write('3. Simulation de production par heure pour 1 panneau')
+    st.write(prod_df)
